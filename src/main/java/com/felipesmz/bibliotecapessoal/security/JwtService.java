e1edb9f5b3e1f615ebff33648b424248410cab17
Felipe <felipeshimizu5@gmail.com>
2026-04-15 17:43:49 -0300

    docs: ajustando o aplication-example.properties para o JWT

---
+++ src/main/java/com/felipesmz/bibliotecapessoal/security/JwtService.java
@@ -0,0 +1,19 @@
+package com.felipesmz.bibliotecapessoal.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration-ms}")
+    private long expirationMs;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+
+}
