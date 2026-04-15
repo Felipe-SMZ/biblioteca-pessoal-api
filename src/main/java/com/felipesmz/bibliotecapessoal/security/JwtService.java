5b3e2419557929a1b2f2cbc12e13c42ce631bfa3
Felipe <felipeshimizu5@gmail.com>
2026-04-15 17:49:14 -0300

    feat: JWTService

--- src/main/java/com/felipesmz/bibliotecapessoal/security/JwtService.java
+++ src/main/java/com/felipesmz/bibliotecapessoal/security/JwtService.java
@@ -1,7 +1,13 @@
 package com.felipesmz.bibliotecapessoal.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.felipesmz.bibliotecapessoal.model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class JwtService {
@@ -15,5 +21,26 @@ public class JwtService {
     @Value("${jwt.issuer}")
     private String issuer;
 
+    public String gerarToken(Usuario usuario) {
+        Instant agora = Instant.now();
+        Instant expiracao = agora.plusMillis(expirationMs);
 
+        return JWT.create()
+                .withSubject(usuario.getEmail())
+                .withIssuer(issuer)
+                .withIssuedAt(agora)
+                .withExpiresAt(expiracao)
+                .sign(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret));
+    }
+
+    public DecodedJWT validarToken(String token) {
+        return JWT.require(Algorithm.HMAC256(secret))
+                .withIssuer(issuer)
+                .build()
+                .verify(token);
+    }
+
+    public long getExpirationMs() {
+        return expirationMs;
+    }
 }
